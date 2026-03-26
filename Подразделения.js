5181a7f3eb6236675a35da03cd006a097e7de517
WOLFbl4 <47863125+WOLFbl4@users.noreply.github.com>
2026-03-26 21:29:36 +0300

    Create Подразделения.js with sample employee divisions data

---
+++ Подразделения.js
@@ -0,0 +1,10 @@
+[
+  {"Division Name": "Buff", "ID": "1", "Boss ID": "null", "Username": "buff_user", "User ID": "001", "Photo": "buff_photo_url"},
+  {"Division Name": "Административное управление", "ID": "2", "Boss ID": "null", "Username": "admin_user", "User ID": "002", "Photo": "admin_photo_url"},
+  {"Division Name": "Бухгалтерия", "ID": "3", "Boss ID": "null", "Username": "accounting_user", "User ID": "003", "Photo": "accounting_photo_url"},
+  {"Division Name": "Дирекция", "ID": "4", "Boss ID": "null", "Username": "director_user", "User ID": "004", "Photo": "director_photo_url"},
+  {"Division Name": "Казначейство", "ID": "5", "Boss ID": "null", "Username": "treasury_user", "User ID": "005", "Photo": "treasury_photo_url"},
+  {"Division Name": "Клиентский сервис", "ID": "6", "Boss ID": "null", "Username": "client_service_user", "User ID": "006", "Photo": "client_service_photo_url"},
+  {"Division Name": "ОПТ", "ID": "7", "Boss ID": "null", "Username": "opt_user", "User ID": "007", "Photo": "opt_photo_url"},
+  {"Division Name": "Отдел маркетинга", "ID": "8", "Boss ID": "null", "Username": "marketing_user", "User ID": "008", "Photo": "marketing_photo_url"}
+]
